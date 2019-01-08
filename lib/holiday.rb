2d49698e4bab0faa39db7393d0eb8a94da060b09
Trent Moss <trentmoss05@gmail.com>
2019-01-08 18:05:54 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,6 +49,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].collect do |holiday, supplies|
     supplies
   end
+  .flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
